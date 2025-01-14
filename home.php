2def699bdbba10395f6b6bcb013b1d36e3ec48ce
gordonporlock <sulvainpeters@gmail.com>
2025-01-14 15:06:06 +0100

    Made basic HTML for the Home recreation (and upgrading) and made minor tweaks to index again

---
+++ home.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RF de Wit Auto's</title>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+    >
+</head>
+<nav>
+  <a href="home.php"> Home </a>
+  <a href=" "> Occasions </a>
+  <a href=" "> Workplace  </a>
+  <a href=" "> Contact </a>
+  <a href=" "> Reserve Your Maintenance </a>
+</nav>
+
+<body>
+
+<section class="subbanner">
+   <p> R. F. De Wit Auto's: "Service is ontzettend belangrijk"</p>
+</section>
+
+<section class="head">
+   <h1> R. F. De Wit Auto's </h1>
+    <p> Voor inkoop en verkoop van jonge gebruikte auto's </p>
+    <a href=" "> Onze Occasions </a>
+</section>
+
+<section class="servicecards">
+  <div class="workplace">
+      <h2> Werkplaats</h2>
+      <p> Heeft uw auto onderhoud nodig? of is er wat defect? In onze werkplaats zullen wij u op vakkundige en professionele wijze weer op weg helpen, voor een apk kun u altijd zonder afspraak terecht. </p>
+      <a href="create.php"> Reserveer Nu</a>
+  </div>
+  <div class="occasions">
+      <h2> Occasions </h2>
+      <p> Met onze werkplaats om de hoek controleren wij elke auto van top tot teen. Zo heeft u niet enkel de keuze uit een mooie auto, maar een occasion die volledig is gecontroleerd. </p>
+      <a href=" "> Bekijk het Aanbod </a>
+  </div>
+   <div>
+      <h2> Contact </h2>
+      <p> Heeft u een vraag aan ons over een occasion of reparatie? Neem via verschillende kanalen contact op om uw vraag beantwoord te krijgen. </p>
+      <a href=" "> Contact met Ons </a>
+   </div>
+</section>
+
+<section class="aboutus">
+    <h2> Een autobedrijf waar service nog écht service is. </h2>
+    <p> Bij R.F. de Wit vinden wij het contact met onze klanten nog altijd het leukste. Service is bij ons het belangrijkste en we merken dat onze klanten dat ook écht zo ervaren. In de afgelopen jaren hebben wij hard gewerkt aan het neerzetten van een volwaardig autobedrijf waar u terecht kunt voor reparaties en onderhoud aan uw auto. Maar bovenal kunt u bij ons ook op zoek naar een nieuwe occassion waar u weer jaren met plezier in kunt rijden.
+        <br>
+        RF de Wit Auto's biedt online casinospelers de beste autoreparatieservice. Daarnaast kan het autobedrijf van RF de Wit Auto niet alleen uw auto onderhouden en repareren, maar ook Voedingsmiddelen die helpen bij je concentratie, wat belangrijk is voor automobilisten. We bieden ook auto's aan van verschillende merken en in alle prijsklassen, dus de online casinospeler heeft genoeg om uit te kiezen.
+        Wij bieden auto’s aan van verschillende merken en binnen allerlei prijsklassen. Kunt u de gedroomde auto niet bij ons vinden? Ook dat is geen enkel probleem. Laat ons u helpen bij uw zoektocht. Wij zetten dan alle zeilen bij om te zoeken naar “die ene auto” waar u blij van wordt.
+        <br>
+        Kiest u ervoor om uw auto bij ons te kopen én het onderhoud door onze vakkundige automonteurs uit te laten voeren, dan kunt u rekenen op een leuke korting </p>
+</section>
+
+//Add section with Map to Location
+</body>
+
+<footer>
+//Insert Footer stuff
+
+</footer>
+
+</html>
